2a1b21389c1ec32345a0b54c0ca48d0cb6c4e71d
Yanik-Dev <yanikblake@live.com>
2017-04-28 16:14:19 -0500

    added comments to configurations.php

--- config/Configurations.php
+++ config/Configurations.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Project Configurations
+ * Contains all the configurations for external services
+ * such as database, uploads folder, etc.
+ *
+ * USAGE: 
+ *  include in your php template or services
+ *  then access the associative arrays by.
+ *  $_CONFIG["DATABASE"]["SERVER"] to get server name
+ */
 $_CONFIG = [
     "DATABASE" => [
         "SERVER" =>"127.0.0.1",
