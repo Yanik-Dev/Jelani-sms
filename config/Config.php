aefdbce8e1514d368c7c827264b00f1cab204772
Yanik-Dev <yanikblake@live.com>
2017-04-28 16:32:13 -0500

    refactor DatabaseService.php

--- config/Config.php
+++ config/Config.php
@@ -6,12 +6,12 @@
  * such as database, uploads folder, etc.
  *
  * USAGE: 
- *  include in your php template or services
+ *  include in your php template or service
  *  then access the associative arrays by.
  *  $_CONFIG["DATABASE"]["SERVER"] to get server name
  */
 $_CONFIG = [
-    "DATABASE" => [
+    "DATABASECONFIG" => [
         "SERVER" =>"127.0.0.1",
         "USERNAME" => "root",
         "PASSWORD" => "",
