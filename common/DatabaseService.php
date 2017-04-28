c16668f8016e617c6c4c5b3fdd2b1c703990e69a
Yanik-Dev <yanikblake@live.com>
2017-04-28 15:53:06 -0500

    added DatabaseService class

---
+++ common/DatabaseService.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Database class
+ * Defines Database connection
+ */
+ class Database{
+    private static $instance = null;
+
+    /**
+     * return connection to database
+     * @return Mysqli Connection
+     */
+    public static function getInstance(){
+        if(self::$instance == null){
+             self::$instance = new Mysqli("localhost", "root", "" , "coding_society");
+            if (self::$instance->connect_errno) {
+                echo "Failed to connect to MySQL: (" . self::$instance->connect_errno . ") " . self::$instance->connect_error;
+            }
+        }
+        return self::$instance;
+    }
+}
+
+
+?>
