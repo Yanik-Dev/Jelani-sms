807d42d116688c4d25f88005e320d3902c84670d
Alcardo Terlonge <alcardodevelopment@gmail.com>
2017-04-28 17:15:48 -0700

    login.php added to template folder

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 
-  <script type="text/javascript" src="./assets/vendor/js/vendor.js"></script>
-  <script type="text/javascript" src="./assets/vendor/js/app.js"></script>
+  <script type="text/javascript" src=".././assets/vendor/js/vendor.js"></script>
+  <script type="text/javascript" src=".././assets/vendor/js/app.js"></script>
 
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -5,13 +5,13 @@
   
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" type="text/css" href="./assets/vendor/css/vendor.css">
-  <link rel="stylesheet" type="text/css" href="./assets/vendor/css/flat-admin.css">
+  <link rel="stylesheet" type="text/css" href=".././assets/vendor/css/vendor.css">
+  <link rel="stylesheet" type="text/css" href=".././assets/vendor/css/flat-admin.css">
 
   <!-- Theme -->
-  <link rel="stylesheet" type="text/css" href="./assets/vendor/theme/blue-sky.css">
-  <link rel="stylesheet" type="text/css" href="./assets/vendor/css/theme/blue.css">
-  <link rel="stylesheet" type="text/css" href="./assets/vendor/css/theme/red.css">
-  <link rel="stylesheet" type="text/css" href="./assets/vendor/css/theme/yellow.css">
+  <link rel="stylesheet" type="text/css" href=".././assets/vendor/theme/blue-sky.css">
+  <link rel="stylesheet" type="text/css" href=".././assets/vendor/css/theme/blue.css">
+  <link rel="stylesheet" type="text/css" href=".././assets/vendor/css/theme/red.css">
+  <link rel="stylesheet" type="text/css" href=".././assets/vendor/css/theme/yellow.css">
 
 </head>
